086d865212c5948890a302cd28d6037bfa675ae1
Ahmed Elshhawy <ahmedelshehawy980@gmail.com>
2026-02-09 19:48:45 +0200

    footer

--- src/app/_component/Footer/Footer.tsx
+++ src/app/_component/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <div className="bg-gray-900 w-full  h-50 px-10 py-10 flex justify-center">
+      <div className="bg-gray-900 w-full  h-50 px-10 py-10 flex justify-center relative bottom-0">
         <p className="text-gray-100 my-10">Ahmed. All Rights Reserved</p>
       </div>
     </>
